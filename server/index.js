84298cb5174ae52d2af26dbaeab4241b9e16bee5
Kelvonetics <33033136+Kelvonetics@users.noreply.github.com>
2025-06-24 15:04:55 +0100

    Updated Index.js

--- server/index.js
+++ server/index.js
@@ -60,13 +60,6 @@ app.use(`/uploads`, express.static(path.join(__dirname, 'uploads')));
 
 __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/client/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-	});
-}
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => {
